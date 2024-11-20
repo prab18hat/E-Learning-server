c4599a26869b0f8959ad81abe4f6286345642817
PRABHAT MISHRA <125785780+prab18hat@users.noreply.github.com>
2024-11-20 00:37:02 -0800

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -40,7 +40,7 @@ export const register = TryCatch(async (req, res) => {
         otp,
     };
 
-    await sendMail(email, "E learning", data);
+    await sendMail(email, "CoddyCulture", data);
 
     res.status(200).json({
         message: "Otp send to your mail",
@@ -122,7 +122,7 @@ export const forgotPassword = TryCatch(async (req, res) => {
 
     const data = { email, token };
 
-    await sendForgotMail("E learning", data);
+    await sendForgotMail("CoddyCulture", data);
 
     user.resetPasswordExpire = Date.now() + 5 * 60 * 1000;
 
